69ddfebf5a9007f4307d3f129a11d96133aa455e
Luigi Cleffi <65309377+LuigiCleffi@users.noreply.github.com>
2023-03-16 12:39:40 -0300

    Atualizar o dbconnect.js

--- trelloBack End/src/config/dbconnect.js
+++ trelloBack End/src/config/dbconnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://luigi:GKO7I19pOl2NSuU3@cluster0.hjpax1r.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://luigi:{password}@cluster0.hjpax1r.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true })
 
 let db = mongoose.connection
 
